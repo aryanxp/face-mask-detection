d4e1fb2679f38ba42c1f89bfc74714e6bb0f9f54
Aryan Pitliya <38190250+aryanxp@users.noreply.github.com>
2021-10-12 11:59:54 +0530

    formatted properly👌✔

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -1,35 +1,15 @@
-# import the necessary packages
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.layers import AveragePooling2D
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Input
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import load_img
-from tensorflow.keras.utils import to_categorical
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from imutils import paths
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-# initialize the initial learning rate, number of epochs to train for,
-# and batch size
 INIT_LR = 1e-4
 EPOCHS = 20
 BS = 32
 DIRECTORY = r"dataset"
 CATEGORIES = ["with_mask", "without_mask"]
 
-# grab the list of images in our dataset directory, then initialize
-# the list of data (i.e., images) and class images
 print("[INFO] loading images...")
 
 data = []
@@ -46,7 +26,8 @@ for category in CATEGORIES:
         data.append(image)
         labels.append(category)
 
-# perform one-hot encoding on the labels
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
@@ -54,10 +35,11 @@ labels = to_categorical(labels)
 data = np.array(data, dtype="float32")
 labels = np.array(labels)
 
+from sklearn.model_selection import train_test_split
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
-                                                  test_size=0.20, stratify=labels, random_state=42)
+                        test_size=0.20, stratify=labels, random_state=42)
 
-# construct the training image generator for data augmentation
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 aug = ImageDataGenerator(
     rotation_range=20,
     zoom_range=0.15,
@@ -67,36 +49,34 @@ aug = ImageDataGenerator(
     horizontal_flip=True,
     fill_mode="nearest")
 
-# load the MobileNetV2 network, ensuring the head FC layer sets are
-# left off
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import Input
 baseModel = MobileNetV2(weights="imagenet", include_top=False,
                         input_tensor=Input(shape=(224, 224, 3)))
 
-# construct the head of the model that will be placed on top of the
-# the base model
 headModel = baseModel.output
+from tensorflow.keras.layers import AveragePooling2D
 headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
+from tensorflow.keras.layers import Flatten
 headModel = Flatten(name="flatten")(headModel)
+from tensorflow.keras.layers import Dense
 headModel = Dense(128, activation="relu")(headModel)
+from tensorflow.keras.layers import Dropout
 headModel = Dropout(0.5)(headModel)
 headModel = Dense(2, activation="softmax")(headModel)
 
-# place the head FC model on top of the base model (this will become
-# the actual model we will train)
+from tensorflow.keras.models import Model
 model = Model(inputs=baseModel.input, outputs=headModel)
 
-# loop over all layers in the base model and freeze them so they will
-# *not* be updated during the first training process
 for layer in baseModel.layers:
     layer.trainable = False
 
-# compile our model
 print("[INFO] compiling model...")
+from tensorflow.keras.optimizers import Adam
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
               metrics=["accuracy"])
 
-# train the head of the network
 print("[INFO] training head...")
 H = model.fit(
     aug.flow(trainX, trainY, batch_size=BS),
@@ -105,23 +85,19 @@ H = model.fit(
     validation_steps=len(testX) // BS,
     epochs=EPOCHS)
 
-# make predictions on the testing set
 print("[INFO] evaluating network...")
+from sklearn.metrics import classification_report
 predIdxs = model.predict(testX, batch_size=BS)
 
-# for each image in the testing set we need to find the index of the
-# label with corresponding largest predicted probability
 predIdxs = np.argmax(predIdxs, axis=1)
 
-# show a nicely formatted classification report
 print(classification_report(testY.argmax(axis=1), predIdxs,
                             target_names=lb.classes_))
 
-# serialize the model to disk
 print("[INFO] saving mask detector model...")
 model.save("mask_detector.model", save_format="h5")
 
-# plot the training loss and accuracy
+import matplotlib.pyplot as plt
 N = EPOCHS
 plt.style.use("ggplot")
 plt.figure()
